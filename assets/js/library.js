5a1a952f01468833138da8b23f88a7446c81c677
Kevin Vuong <c_k.k@hotmail.com>
2019-12-06 21:11:08 +1100

    addBookToLibrary function

--- assets/js/library.js
+++ assets/js/library.js
@@ -6,6 +6,14 @@ function Book(title, author, pages, read) {
   this.pages = pages
   this.read = read
   this.info = function() {
-    return `${this.title}, ${this.author}, ${this.pages}, ${this.read}`;
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
   }
 }
+
+function addBookToLibrary(title, author, pages, read) {
+  let new_book = new Book(title, author, pages, read);
+  myLibrary.push(new_book);
+}
+
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, "not read yet");
+console.log(myLibrary);
